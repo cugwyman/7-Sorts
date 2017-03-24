615d76d130dd5d15c651762d9dba465d7f1c461c
cugwyman <435059696@qq.com>
2017-03-25 01:21:48 +0800

    7 sorts in c/c++
    通过VS2015调试的七大基本排序算法。

---
+++ Sorts/main.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include "Sort.h"
+
+
+int main(void)
+{
+	int a[len] = { 5,9,7,4,6,3,2,10,8,1 };
+	//BubbleSort(a);
+	//ShellSort(a);
+	//SelectSort(a);
+	//InsertSort(a);
+	//QuickSort(a);
+	//MergeSort(a);
+	HeapSort(a);
+	PrintArr(a);
+	system("pause");
+	return 0;
+}
+
